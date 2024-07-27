6bdf012c12e469d050caf0b5985aed6558cf783b
kitty <3404593235@qq.com>
2024-07-27 11:18:18 +0800

    验证token---xue

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module studentGrow
 go 1.21
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-redis/redis v6.15.9+incompatible
---
+++ utils/token/reToken.go
@@ -0,0 +1,42 @@
+package token
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"strings"
+	"studentGrow/models"
+)
+
+// 中间件检验token是否合法
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 获取前端传过来的信息
+		tokenString := c.GetHeader("token")
+		fmt.Print("请求token", tokenString)
+		//验证前端传过来的token格式，不为空，开头为Bearer
+		if tokenString == "" || !strings.HasPrefix(tokenString, "Bearer ") {
+			c.JSON(400, gin.H{"code": "400", "msg": "验证失败"})
+			return
+		}
+		//验证通过，提取有效部分（除去Bearer)
+		tokenString = tokenString[7:] //截取字符
+		//解析token
+		token, _, err := ParseToken(tokenString)
+		//解析失败||解析后的token无效
+		if err != nil || !token.Valid {
+			c.JSON(400, gin.H{"code": "400", "msg": "验证失败"})
+			return
+		}
+		c.Next()
+	}
+}
+
+// ParseToken 解析从前端获取到的token值
+func ParseToken(tokenString string) (*jwt.Token, *models.Claims, error) {
+	claims := &models.Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	return token, claims, err
+}
