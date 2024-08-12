6c08525b6678a957de572241e0aee3e329f1ad9c
ningzhaoxing <144292794+ningzhaoxing@users.noreply.github.com>
2024-08-12 08:33:59 +0800

    后台帖子管理、举报信箱 前台模糊搜索帖子、点赞收藏

--- dao/mysql/sql_nzx.go
+++ dao/mysql/sql_nzx.go
@@ -46,45 +46,6 @@ func SelectArticleById(aid int) (err error, article *model.Article) {
 	}
 }
 
-// InsertIntoCommentsForArticle 向数据库插入评论数据(回复文章)
-
-func InsertIntoCommentsForArticle(content string, aid int, uid int) (err error) {
-	//content;id;username
-	//comment := model.Comment{
-	//	Model:        gorm.Model{},
-	//	Content:      content,
-	//	UpvoteAmount: 0,
-	//	IsRead:       false,
-	//	Del:          false,
-	//	Uid:          uid,
-	//	Pid:          0,
-	//	Aid:          aid,
-	//	Upvote:       nil,
-	//}
-	//DB.Create(&comment)
-
-	return nil
-}
-
-// InsertIntoCommentsForComment 向数据库插入评论数据(回复评论)
-func InsertIntoCommentsForComment(content string, uid int, pid int) (err error) {
-	//content;id;username
-	//comment := model.Comment{
-	//	Model:        gorm.Model{},
-	//	Content:      content,
-	//	UpvoteAmount: 0,
-	//	IsRead:       false,
-	//	Del:          false,
-	//	Uid:          uid,
-	//	Pid:          pid,
-	//	Aid:          0,
-	//	Upvote:       nil,
-	//}
-	//
-	//DB.Create(&comment)
-	return nil
-}
-
 // SelectArticleAndUserListByPage 后台分页查询文章及用户列表并模糊查询
 func SelectArticleAndUserListByPage(page, limit int, sort, order, startAt, endAt, topic, keyWords, name string, isBan bool) (result []model.Article, err error) {
 	//SELECT articles.*, users.*
