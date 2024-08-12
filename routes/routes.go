4834bd3035c2560c3b866c912a9edb209d552718
gumnbum <1962901241@qq.com>
2024-08-12 21:07:02 +0800

    加跨域

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,7 @@ import (
 func Setup() *gin.Engine {
 	r := gin.New()
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	r.Use(middleWare.CORSMiddleware())
 	// 勋
 	r.POST("/student/getSelfCotnent", student.GetSelfContentContro)
 	r.POST("/student/updateSelfContent", student.UpdateSelfContentContro)
