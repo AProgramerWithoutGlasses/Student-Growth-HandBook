60dc72be572b5b2cc04ea88ba44828c5e31c2e23
kitty <3404593235@qq.com>
2024-08-13 14:34:39 +0800

    加跨域

--- routes/routesXue.go
+++ routes/routesXue.go
@@ -32,7 +32,7 @@ func RoutesXue(router *gin.Engine) {
 		userLoginAfter.POST("/fpage/grade", login.FPageGrade)
 		userLoginAfter.POST("/fpage/college", login.FPageCollege)
 		userLoginAfter.POST("/fpage/superman", login.FPageCollege)
-		userLoginAfter.POST("/fpage/pillar", login.Pillar)
+		userLoginAfter.GET("/fpage/pillar", login.Pillar)
 	}
 
 }
