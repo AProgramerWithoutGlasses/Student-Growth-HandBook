bb47992f4ee2e80927aad1227aad8488dd100835
gumnbum <1962901241@qq.com>
2024-08-19 11:19:21 +0800

    个人主页修改头像

---
+++ models/constant/constant.go
@@ -0,0 +1,45 @@
+package constant
+
+// PointConstant 积分常量
+const PointConstant = 5
+
+/*
+热度权重
+*/
+
+// LikeWeightConstant 点赞
+const LikeWeightConstant = 0.5
+
+// CollectWeightConstant 收藏
+const CollectWeightConstant = 0.2
+
+// CommentWeightConstant 评论
+const CommentWeightConstant = 0.3
+
+/*
+互动类型
+*/
+
+// ArticleInteractionConstant 文章互动
+const ArticleInteractionConstant = 0
+
+// CommentInteractionConstant 评论互动
+const CommentInteractionConstant = 1
+
+/*
+互动消息通知类型
+*/
+
+const LikeMsgConstant = 0
+
+const CommentMsgConstant = 1
+
+const CollectMsgConstant = 2
+
+/*
+广播消息通知类型
+*/
+
+const SystemMsgConstant = 1
+
+const ManagerMsgConstant = 2
