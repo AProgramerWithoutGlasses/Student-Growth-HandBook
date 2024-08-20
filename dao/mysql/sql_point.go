b526dafda5e9e86cc698f292a71b6790d696aee9
ningzhaoxing <144292794+ningzhaoxing@users.noreply.github.com>
2024-08-20 16:02:40 +0800

    增加发布文章增加用户话题分数记录功能

--- dao/mysql/sql_point.go
+++ dao/mysql/sql_point.go
@@ -1,6 +1,7 @@
 package mysql
 
 import (
+	"errors"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 	"studentGrow/models/gorm_model"
@@ -39,4 +40,29 @@ func QueryUserAllPoint(uid int) ([]gorm_model.UserPoint, error) {
 		return nil, myErr.NotFoundError()
 	}
 	return points, nil
+}
+
+// QueryUserPointOfTopicIsExist 查询用户是否存在该话题的积分
+func QueryUserPointOfTopicIsExist(uid, topicId int) (bool, error) {
+	if err := DB.Where("topic_id = ? and user_id = ?", topicId, uid).First(&gorm_model.UserPoint{}).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+// CreateUserPointOfTopic 创建用户话题分数
+func CreateUserPointOfTopic(uid, topicId int, db *gorm.DB) error {
+	point := gorm_model.UserPoint{
+		UserID:  uint(uid),
+		TopicID: uint(topicId),
+		Point:   0,
+	}
+	if err := db.Create(&point).Error; err != nil {
+		zap.L().Error("QueryUserAllPoint() dao.mysql.sql_point.First err=", zap.Error(err))
+		return err
+	}
+	return nil
 }
