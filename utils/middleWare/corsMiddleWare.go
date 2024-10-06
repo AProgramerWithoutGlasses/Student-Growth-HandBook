b7a38d364781f57d8d9b75b2093d7a680f1fe6a3
gumnbum <1962901241@qq.com>
2024-10-06 18:03:42 +0800

    --

--- utils/middleWare/corsMiddleWare.go
+++ utils/middleWare/corsMiddleWare.go
@@ -3,7 +3,6 @@ package middleWare
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"net/http"
 	"strings"
 )
@@ -18,10 +17,10 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// 定义一个允许跨域的域名列表
 		var allowedOrigins = map[string]bool{
-			viper.GetString("origin.dev.h5"):    true,
-			viper.GetString("origin.dev.admin"): true,
-			viper.GetString("origin.pro.h5"):    true,
-			viper.GetString("origin.pro.admin"): true,
+			"http://192.168.10.7":    true,
+			"http://192.168.10.7:81": true,
+			"http://8.154.36.180":    true,
+			"http://8.154.36.180:81": true,
 		}
 
 		method := c.Request.Method               //请求方法
@@ -36,6 +35,9 @@ func CORSMiddleware() gin.HandlerFunc {
 		} else {
 			headerStr = "access-control-allow-origin, access-control-allow-headers"
 		}
+
+		fmt.Println("len map", len(allowedOrigins))
+		fmt.Println("map", allowedOrigins)
 
 		// 检查请求的Origin是否在允许的域名列表中
 		if allowedOrigins[origin] {
