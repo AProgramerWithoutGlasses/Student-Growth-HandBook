c6d5de23e70895dd929132ddb0f13a8a4e3a3f48
gumnbum <1962901241@qq.com>
2024-11-22 08:25:06 +0800

    修复删除学生的bug

--- controller/stuManage/AddMultipleStuController.go
+++ controller/stuManage/AddMultipleStuController.go
@@ -44,83 +44,128 @@ func AddMultipleStuControl(c *gin.Context) {
 
 	duplicatedUser := make([]string, 0)
 
+	var existedNum int
+	var unExistedNum int
+
 	// 检查数据库中是否已经存在该用户
 	for _, row := range rows[1:] { // 忽略表头行
 		err = mysql.ExistedUsername(row[2])
-		fmt.Println("1111122222")
 		if err != nil {
 			// 用户不存在
 			if errors.Is(err, gorm.ErrRecordNotFound) {
-				err = nil
-				continue
+				//err = nil
+				//continue
+
+				unExistedNum++
+
+				// 导入
+				yearInt, err := strconv.Atoi(row[0][6:8])
+				if err != nil {
+					response.ResponseError(c, response.ServerErrorCode)
+					zap.L().Error(err.Error())
+					return
+				}
+
+				if len(row[0]) > 9 {
+					row[0] = row[0][0:9]
+				}
+				user1 := gorm_model.User{
+					Class:    row[0],
+					Name:     row[1],
+					Username: row[2],
+					Password: row[3],
+					Gender:   row[4],
+					Identity: "学生",
+					PlusTime: time.Date(yearInt+2000, 9, 1, 0, 0, 0, 0, time.Now().Location()),
+					HeadShot: "https://student-grow.oss-cn-beijing.aliyuncs.com/image/user_headshot/user_headshot_1.png",
+				}
+				err = mysql.AddSingleStudent(&user1)
+				if err != nil {
+					response.ResponseErrorWithMsg(c, 500, "stuManage.AddMultipleStuControl() mysql.AddSingleStudent failed: "+err.Error())
+					zap.L().Error("stuManage.AddMultipleStuControl() mysql.AddSingleStudent failed: " + err.Error())
+					return
+				}
+
+				// 添加学生记录
+				addUserRecord := gorm_model.UserAddRecord{
+					Username:    user.Username,
+					AddUsername: row[2],
+				}
+				err = mysql.AddSingleStudentRecord(&addUserRecord)
+				if err != nil {
+					response.ResponseError(c, response.ServerErrorCode)
+					zap.L().Error("stuManage.AddMultipleStuControl() mysql.AddSingleStudentRecord() failed: " + err.Error())
+					return
+				}
+
 			} else {
 				response.ResponseErrorWithMsg(c, 500, "stuManage.AddMultipleStuControl() mysql.ExistedUsername() failed: "+err.Error())
 				zap.L().Error("stuManage.AddMultipleStuControl() mysql.ExistedUsername() failed: " + err.Error())
 				return
 			}
-
-		} // 用户存在
-		duplicatedUser = append(duplicatedUser, row[2])
-
-	}
-
-	fmt.Println("检测结束")
-
-	var duplicatedUserStr string
-	if len(duplicatedUser) > 0 {
-		for _, v := range duplicatedUser {
-			duplicatedUserStr = duplicatedUserStr + v + ", "
-		}
-		duplicatedUserStr = duplicatedUserStr[:len(duplicatedUserStr)-2]
-		response.ResponseErrorWithMsg(c, 500, "导入失败，请不要导入已存在的学生学号: "+duplicatedUserStr)
-		zap.L().Error("导入失败，请不要导入已存在的学生学号: " + duplicatedUserStr)
-		return
-	}
-
-	// 创建新的用户
-	for _, row := range rows[1:] {
-		yearInt, err := strconv.Atoi(row[0][6:8])
-		if err != nil {
-			response.ResponseError(c, response.ServerErrorCode)
-			zap.L().Error(err.Error())
-			return
-		}
-
-		if len(row[0]) > 9 {
-			row[0] = row[0][0:9]
-		}
-		user1 := gorm_model.User{
-			Class:    row[0],
-			Name:     row[1],
-			Username: row[2],
-			Password: row[3],
-			Gender:   row[4],
-			Identity: "学生",
-			PlusTime: time.Date(yearInt+2000, 9, 1, 0, 0, 0, 0, time.Now().Location()),
-			HeadShot: "https://student-grow.oss-cn-beijing.aliyuncs.com/image/user_headshot/user_headshot_1.png",
-		}
-		err = mysql.AddSingleStudent(&user1)
-		if err != nil {
-			response.ResponseErrorWithMsg(c, 500, "stuManage.AddMultipleStuControl() mysql.AddSingleStudent failed: "+err.Error())
-			zap.L().Error("stuManage.AddMultipleStuControl() mysql.AddSingleStudent failed: " + err.Error())
-			return
-		}
-
-		// 添加学生记录
-		addUserRecord := gorm_model.UserAddRecord{
-			Username:    user.Username,
-			AddUsername: row[2],
-		}
-		err = mysql.AddSingleStudentRecord(&addUserRecord)
-		if err != nil {
-			response.ResponseError(c, response.ServerErrorCode)
-			zap.L().Error("stuManage.AddMultipleStuControl() mysql.AddSingleStudentRecord() failed: " + err.Error())
-			return
+		} else {
+			// 用户存在
+			existedNum++
+			duplicatedUser = append(duplicatedUser, row[2])
 		}
 
 	}
 
-	addStuNumber := strconv.Itoa(len(rows[1:]))
-	response.ResponseSuccessWithMsg(c, "已导入"+addStuNumber+" 名学生的信息！", nil)
+	//var duplicatedUserStr string
+	//if len(duplicatedUser) > 0 {
+	//	for _, v := range duplicatedUser {
+	//		duplicatedUserStr = duplicatedUserStr + v + ", "
+	//	}
+	//	duplicatedUserStr = duplicatedUserStr[:len(duplicatedUserStr)-2]
+	//	response.ResponseErrorWithMsg(c, 500, "导入失败，请不要导入已存在的学生学号: "+duplicatedUserStr)
+	//	zap.L().Error("导入失败，请不要导入已存在的学生学号: " + duplicatedUserStr)
+	//	return
+	//}
+
+	//// 创建新的用户
+	//for _, row := range rows[1:] {
+	//	yearInt, err := strconv.Atoi(row[0][6:8])
+	//	if err != nil {
+	//		response.ResponseError(c, response.ServerErrorCode)
+	//		zap.L().Error(err.Error())
+	//		return
+	//	}
+	//
+	//	if len(row[0]) > 9 {
+	//		row[0] = row[0][0:9]
+	//	}
+	//	user1 := gorm_model.User{
+	//		Class:    row[0],
+	//		Name:     row[1],
+	//		Username: row[2],
+	//		Password: row[3],
+	//		Gender:   row[4],
+	//		Identity: "学生",
+	//		PlusTime: time.Date(yearInt+2000, 9, 1, 0, 0, 0, 0, time.Now().Location()),
+	//		HeadShot: "https://student-grow.oss-cn-beijing.aliyuncs.com/image/user_headshot/user_headshot_1.png",
+	//	}
+	//	err = mysql.AddSingleStudent(&user1)
+	//	if err != nil {
+	//		response.ResponseErrorWithMsg(c, 500, "stuManage.AddMultipleStuControl() mysql.AddSingleStudent failed: "+err.Error())
+	//		zap.L().Error("stuManage.AddMultipleStuControl() mysql.AddSingleStudent failed: " + err.Error())
+	//		return
+	//	}
+	//
+	//	// 添加学生记录
+	//	addUserRecord := gorm_model.UserAddRecord{
+	//		Username:    user.Username,
+	//		AddUsername: row[2],
+	//	}
+	//	err = mysql.AddSingleStudentRecord(&addUserRecord)
+	//	if err != nil {
+	//		response.ResponseError(c, response.ServerErrorCode)
+	//		zap.L().Error("stuManage.AddMultipleStuControl() mysql.AddSingleStudentRecord() failed: " + err.Error())
+	//		return
+	//	}
+	//
+	//}
+
+	//addStuNumber := strconv.Itoa(len(rows[1:]))
+	response.ResponseSuccessWithMsg(c, fmt.Sprintf("已导入%d条学生信息，剩余%d条未导入，因为其已存在"), nil)
 
 }
