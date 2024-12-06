0f2442c294ba774ee3701af0c42fa762da8a815f
aoge666 <1409050584@qq.com>
2024-12-06 15:25:57 +0800

    入团申请表单导出

--- controller/routesJoinAudit/exportFormMsg.go
+++ controller/routesJoinAudit/exportFormMsg.go
@@ -8,6 +8,7 @@ import (
 	"github.com/nguyenthenguyen/docx"
 	"go.uber.org/zap"
 	"io"
+	"io/fs"
 	"net/url"
 	"os"
 	"strconv"
@@ -53,7 +54,6 @@ func ExportFormMsg(c *gin.Context) {
 		ListName: cr.CurMenu,
 		IsFinish: isFinish,
 	})
-
 }
 func ExportFormFile(c *gin.Context) {
 	token := token2.NewToken(c)
@@ -71,98 +71,38 @@ func ExportFormFile(c *gin.Context) {
 	}
 	_, _, activityMsg := mysql.OpenActivityStates()
 	title := activityMsg.ActivityName
-	if title == "" {
-		title = "通过名单"
+	switch cr.CurMenu {
+	case "organizer":
+		title = title + "积极分子审查表"
+	case "organizerFinish":
+		title = title + "积极分子培训考试成绩审查表"
+	default:
+		title = "入团积极分子审查表"
 	}
 	userMsgList, classList := mysql.UserListWithOrganizer(cr.ActivityID, cr.CurMenu)
-	responseDOCX(userMsgList, c, activityMsg.ActivityName, classList)
+	responseDOCX(userMsgList, c, title, classList)
 	return
 }
 
-//func UseWordTemplateMakeDocx(title string, List []map[string]interface{}, classList []string) (filePath string) {
-//	classified := rewriteTemplate(List)
-//
-//	r, err := docx.ReadDocxFile("controller/routesJoinAudit/word/rewriteTemplate.docx")
-//	if err != nil {
-//		zap.L().Error(err.Error())
-//	}
-//	defer r.Close()
-//	docx1 := r.Editable()
-//	var classNameList []string
-//	for k, v := range classList {
-//		classStr := "CLASS"
-//		nameStr := "NAME"
-//		classStr = "{{" + classStr + strconv.Itoa(k) + "}}"
-//		nameStr = "{{" + nameStr + strconv.Itoa(k) + "}}"
-//		err = docx1.Replace(classStr, v+":", -1)
-//		if err != nil {
-//			zap.L().Error(err.Error())
-//		}
-//		classNameList = make([]string, 0)
-//		for _, value := range classified[v] {
-//			userName, _ := value["name"].(string)
-//			classNameList = append(classNameList, userName)
-//		}
-//		rewriteList := strings.Join(classNameList, "\t")
-//		err = docx1.Replace(nameStr, rewriteList, -1)
-//		if err != nil {
-//			zap.L().Error(err.Error())
-//		}
-//	}
-//	err = docx1.Replace("{{Title}}", title, -1)
-//	if err != nil {
-//		zap.L().Error(err.Error())
-//	}
-//	filePath = "controller/routesJoinAudit/word/" + fmt.Sprintf("%s", title) + ".docx"
-//	err = docx1.WriteToFile(filePath)
-//	if err != nil {
-//		zap.L().Error(err.Error())
-//	}
-//	return
-//}
-
 func responseDOCX(dynamicList []map[string]interface{}, c *gin.Context, title string, classList []string) {
-	classified := make(map[string][]map[string]interface{})
-	for _, v := range dynamicList {
-		userClass, _ := v["user_class"].(string)
-		classified[userClass] = append(classified[userClass], v)
-	}
-	n := len(classified)
-	replaceNameList := make([]string, 0)
-	for i := 0; i < n; i++ {
-		classStr := "CLASS"
-		nameStr := "NAME"
-		classStr = "{{" + classStr + strconv.Itoa(i) + "}}"
-		nameStr = "{{" + nameStr + strconv.Itoa(i) + "}}"
-		replaceNameList = append(replaceNameList, "\n"+classStr)
-		replaceNameList = append(replaceNameList, nameStr)
-	}
-	rewriteList := strings.Join(replaceNameList, "\n")
+	rewriteList, classified := classifyWordListWithClass(dynamicList)
 	// 读取模板文件
-	//读取文件夹dir
 	templateFile, _ := data.Open("template.docx")
-	defer templateFile.Close()
-	// 创建新文件
-	newFile, err := os.Create("./template.docx")
-	if err != nil {
-		zap.L().Error(err.Error())
-	}
-	defer newFile.Close()
-
-	// 将嵌入文件的内容复制到新文件中
-	_, err = io.Copy(newFile, templateFile)
-	if err != nil {
-		zap.L().Error(err.Error())
-	}
-	//entry为遍历dir文件夹中的每个文件
-
+	defer func() {
+		_ = templateFile.Close()
+	}()
+	//复制文件到当前目录下
+	copyFileToPath(templateFile, "./template.docx")
+	//读取模版
 	r, err := docx.ReadDocxFile("./template.docx")
 	if err != nil {
 		response.ResponseErrorWithMsg(c, response.ServerErrorCode, "文件打开失败")
 		zap.L().Error(err.Error())
 		return
 	}
-	defer r.Close()
+	defer func() {
+		_ = r.Close()
+	}()
 	// 使文档可编辑
 	docx1 := r.Editable()
 	// 替换模板中的占位符
@@ -170,9 +110,9 @@ func responseDOCX(dynamicList []map[string]interface{}, c *gin.Context, title st
 	if err != nil {
 		zap.L().Error(err.Error())
 	}
-	fmt.Println(rewriteList)
-	// 将修改后的文档写入新文件
 	var classNameList []string
+
+	//循环遍历学生名单填入对应的list中
 	for k, v := range classList {
 		classStr := "CLASS"
 		nameStr := "NAME"
@@ -187,8 +127,8 @@ func responseDOCX(dynamicList []map[string]interface{}, c *gin.Context, title st
 			userName, _ := value["name"].(string)
 			classNameList = append(classNameList, userName)
 		}
-		rewriteList := strings.Join(classNameList, "\t")
-		err = docx1.Replace(nameStr, rewriteList, -1)
+		rewriteNameList := strings.Join(classNameList, "\t")
+		err = docx1.Replace(nameStr, rewriteNameList, -1)
 		if err != nil {
 			zap.L().Error(err.Error())
 		}
@@ -198,7 +138,10 @@ func responseDOCX(dynamicList []map[string]interface{}, c *gin.Context, title st
 		zap.L().Error(err.Error())
 	}
 	filename := url.QueryEscape(title + ".docx")
+
+	//添加响应头
 	c.Writer.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename*=utf-8''%s", filename))
+	//将结果写到响应中
 	err = docx1.Write(c.Writer)
 	if err != nil {
 		zap.L().Error(err.Error())
@@ -206,30 +149,40 @@ func responseDOCX(dynamicList []map[string]interface{}, c *gin.Context, title st
 	return
 }
 
-// func Demo(c *gin.Context) {
-//
-//	r, err := docx.ReadDocxFile("controller/routesJoinAudit/word/template.docx")
-//	if err != nil {
-//		log.Printf("docx Err %v", err)
-//	}
-//	defer func() {
-//		_ = r.Close()
-//	}()
-//
-//	// 使文档可编辑
-//	docx1 := r.Editable()
-//	// 替换模板中的占位符
-//	err = docx1.Replace("{{LIST}}", "rewriteList\n asdfasdf", -1)
-//	err = docx1.Replace("{{Title}}", "入团申请第十一期", -1)
-//	if err != nil {
-//		log.Printf("Replace Err %v", err)
-//	}
-//
-//	c.Writer.Header().Set("Content-Disposition", `attachment; filename="tes.docx"`)
-//
-//	err = docx1.Write(c.Writer)
-//	if err != nil {
-//		log.Printf("Write Err %v", err)
-//	}
-//
-// }
+// 根据班级把查询到的名字分类
+func classifyWordListWithClass(dynamicList []map[string]interface{}) (rewriteList string, classified map[string][]map[string]interface{}) {
+	classified = make(map[string][]map[string]interface{})
+	for _, v := range dynamicList {
+		userClass, _ := v["user_class"].(string)
+		classified[userClass] = append(classified[userClass], v)
+	}
+	n := len(classified)
+	replaceNameList := make([]string, 0)
+	for i := 0; i < n; i++ {
+		classStr := "CLASS"
+		nameStr := "NAME"
+		classStr = "{{" + classStr + strconv.Itoa(i) + "}}"
+		nameStr = "{{" + nameStr + strconv.Itoa(i) + "}}"
+		replaceNameList = append(replaceNameList, "\n"+classStr)
+		replaceNameList = append(replaceNameList, nameStr)
+	}
+	rewriteList = strings.Join(replaceNameList, "\n")
+	return
+}
+
+// 复制打包到可执行文件中的文件到同级目录中
+func copyFileToPath(templateFile fs.File, path string) {
+	newFile, err := os.Create(path)
+	if err != nil {
+		zap.L().Error(err.Error())
+	}
+	defer func() {
+		_ = newFile.Close()
+	}()
+
+	_, err = io.Copy(newFile, templateFile)
+	if err != nil {
+		zap.L().Error(err.Error())
+	}
+	return
+}
